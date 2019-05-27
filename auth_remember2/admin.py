a3de66aad039128490964c90d596f3431e19680d
David Kremer <david.kremer@sporteasy.net>
2019-05-27 19:10:43 +0200

    use a relative import for app models

--- auth_remember2/admin.py
+++ auth_remember2/admin.py
@@ -3,4 +3,15 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import RememberToken
+
+
+class RememberTokenAdmin(admin.ModelAdmin):
+    class Meta:
+        app_label = 'auth_remember'
+        db_table = 'remembertokenadmin'
+
+    list_display = ('user', 'token_hash', 'created', 'created_initial')
+
+
+admin.site.register(RememberToken, RememberTokenAdmin)
