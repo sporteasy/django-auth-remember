062cbc846dd7f0dd7b1cd6ee8f7875b6de069eae
David Kremer <david.kremer@sporteasy.net>
2019-05-27 17:17:55 +0200

    Revert "import inside the function to avoid problem"
    This reverts commit 94e440040d9f1ff3ec5b08db79416735906fe410.

--- auth_remember/__init__.py
+++ auth_remember/__init__.py
@@ -1,3 +1,6 @@
+from auth_remember.utils import create_token_string
+from auth_remember.utils import preset_cookie
+
 
 def remember_user(request, user):
     """Set the remember-me flag on the user.
@@ -6,9 +9,5 @@ def remember_user(request, user):
     This token is set as a cookie value by the middleware.
 
     """
-
-    from auth_remember.utils import create_token_string
-    from auth_remember.utils import preset_cookie
-
     token_string = create_token_string(user, None)
     preset_cookie(request, token_string)
