94e440040d9f1ff3ec5b08db79416735906fe410
David Kremer <david.kremer@sporteasy.net>
2019-05-27 17:11:14 +0200

    import inside the function to avoid problem

--- auth_remember/__init__.py
+++ auth_remember/__init__.py
@@ -1,6 +1,3 @@
-from auth_remember.utils import create_token_string
-from auth_remember.utils import preset_cookie
-
 
 def remember_user(request, user):
     """Set the remember-me flag on the user.
@@ -9,5 +6,9 @@ def remember_user(request, user):
     This token is set as a cookie value by the middleware.
 
     """
+
+    from auth_remember.utils import create_token_string
+    from auth_remember.utils import preset_cookie
+
     token_string = create_token_string(user, None)
     preset_cookie(request, token_string)
